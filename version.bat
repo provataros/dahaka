1722fa75fefc9d9f832ce4d5db8e4909f1179eff
provataros <provataros@hotmail.com>
2017-04-07 16:18:11 +0300

    versioning

---
+++ version.bat
@@ -0,0 +1,9 @@
+cd dahaka-auth
+call npm version patch --no-git
+cd ../dahaka-expenses
+call npm version patch --no-git
+cd ../dahaka-external
+call npm version patch --no-git
+cd ..
+call npm version patch --no-git
+pause
